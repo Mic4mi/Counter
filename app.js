cae4658e084373bf97ee06ec30bb7ae710be1e66
Mic4mi <61918959+Mic4mi@users.noreply.github.com>
2020-03-07 19:29:42 -0300

    Third commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var btnClickear = document.getElementById('btnclick');
+var btnResetear = document.getElementById('brnResetear');
+var counter = document.getElementById('countContent');
+var numCounter = 0;
+
